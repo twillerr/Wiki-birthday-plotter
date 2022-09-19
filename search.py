77ed7320b13daffecf5e6c3d41c28661eacb8993
twillerr <willowf2001@gmail.com>
2022-09-19 15:25:11 +0100

    added a script containing search func

---
+++ search.py
@@ -0,0 +1,43 @@
+import json
+
+with open("twice_info.json", "r") as f:
+    members = json.load(f)
+    members2 = members
+
+
+print("This dictionary contains the birthdays of:")
+for i in list(members.keys()):
+    print(i)
+    
+while True:
+    action = input("would you like to view or add entries? ")
+
+    #update members from file
+    with open("twice_info.json", "r") as f:
+        members = json.load(f)
+    
+    
+    if action == "view":
+        try:
+            qry = input("Enter a name to find birthday: ").capitalize()
+            print("{name}'s birthday is {date}.".format(name=qry,
+                                                        date=members[qry]))
+        except KeyError:
+            print("Sorry, we can't find that name. Try again.")
+                
+    elif action == "add":
+        name = input("Enter member name:").capitalize()
+        birthday = input("Enter the birthday (d/m/yyyy):")
+        members2[name] = birthday
+
+        with open("twice_info.json", "w") as f:
+            try:
+                json.dump(members2,f)
+            except:
+                json.dump(members,f)
+                print("error in writing. reverted to original")
+                
+
+   
+
+
